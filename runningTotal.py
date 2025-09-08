cfdebe32e7e0da48bb5079f1d5d002638a770b49
KacperBondyra <ctw21.kacper.bondyra@corketb.ie>
2025-09-08 13:58:44 +0100

    Add files via upload

---
+++ runningTotal.py
@@ -0,0 +1,15 @@
+# Program to calculate a running total
+
+#intialize the variable
+runningTotal = 0
+
+# Perform the calculations
+price1 = 10
+runningTotal = runningTotal + price1
+price2 = 14
+runningTotal = runningTotal + price2
+price3 = 6
+runningTotal = runningTotal + price3
+
+# Display the output
+print("Total amount is", runningTotal)
